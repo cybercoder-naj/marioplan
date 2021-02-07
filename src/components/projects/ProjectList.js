9d34c498801a52488b793935363c5de962cccdd4
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-02-07 20:28:52 +0530

    Added ProjectSummary.js code

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,31 +1,13 @@
 import React from 'react';
+import ProjectSummary from "./ProjectSummary";
 
 const ProjectList = () => {
     return (
         <div className="project-list section">
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Cybercoder</p>
-                    <p className="grey-text">7th February, 2021</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Cybercoder</p>
-                    <p className="grey-text">7th February, 2021</p>
-                </div>
-            </div>
-
-            <div className="card z-depth-0 project-summary">
-                <div className="card-content grey-text darken-3">
-                    <span className="card-title">Project Title</span>
-                    <p>Posted by Cybercoder</p>
-                    <p className="grey-text">7th February, 2021</p>
-                </div>
-            </div>
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
+            <ProjectSummary />
         </div>
     );
 };
