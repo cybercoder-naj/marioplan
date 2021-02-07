38e28bc933b856a0405fc0206298fe609b6fca0f
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-02-07 19:57:48 +0530

    Setting up the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-function App() {
-  return (
-    <div className="App">
+import React, {Component} from "react/cjs/react.production.min";
+import { BrowserRouter } from "react-router-dom";
 
-    </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <BrowserRouter>
+                <div className="App">
+                    <h1>MarioPlan</h1>
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
