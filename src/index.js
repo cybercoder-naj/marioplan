1dccc9fbbcf85997707ffaf64753c35df068b947
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-02-09 13:07:32 +0530

    Connected Firebase and Firestore to Redux

--- src/index.js
+++ src/index.js
@@ -3,12 +3,41 @@ import ReactDOM from 'react-dom';
 import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore, applyMiddleware} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import {Provider} from 'react-redux';
 import thunk from "redux-thunk";
+import {reduxFirestore, getFirestore} from "redux-firestore";
+import {getFirebase, createFirebaseInstance, ReactReduxFirebaseProvider} from "react-redux-firebase";
+import fbConfig from "./config/fbConfig";
+import firebase from "firebase/app";
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+    rootReducer,
+    compose(
+        applyMiddleware(thunk.withExtraArgument(
+            {
+                getFirebase,
+                getFirestore
+            })
+        ),
+        reduxFirestore(firebase, fbConfig),
+    )
+);
 
-ReactDOM.render(<Provider store={store}><App/></Provider>, document.getElementById('root'));
+const rrfProps = {
+    firebase,
+    config: fbConfig,
+    dispatch: store.dispatch,
+    createFirebaseInstance
+}
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ReactReduxFirebaseProvider {...rrfProps}>
+            <App/>
+        </ReactReduxFirebaseProvider>
+    </Provider>,
+    document.getElementById('root')
+);
 reportWebVitals();
--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,7 +1,7 @@
 import {addProject} from './types.js';
 
 export const createProject = project => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, {getFirebase, getFirestore}) => {
         // TODO make async call to database
         dispatch({
             type: addProject,
